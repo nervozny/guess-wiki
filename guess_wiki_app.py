3ff1bb8ebf8ba7dc291b82834b1a4861bd8734c8
nervozny <amokshanov@gmail.com>
2022-09-12 18:08:29 +0300

    add tagger

--- guess_wiki_app.py
+++ guess_wiki_app.py
@@ -13,6 +13,7 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
